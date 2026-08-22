028cdfbedffbee61b48f3a16c4b44df64f136eed
Kaif Ansari <ansarikaif51692@gmail.com>
2026-08-23 00:06:37 +0530

    Added Python OOPs concepts

--- OOPs-Intro.cpp
+++
@@ -1,2 +0,0 @@
-// To map with real world scenarios, we started using objects in code. This is called object oriented programming system.
-
---
+++ OOPs-Intro.py
@@ -0,0 +1,2 @@
+# To map with real world scenarios, we started using objects in code. This is called object oriented programming system.
+
