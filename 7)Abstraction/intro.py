9b27a3f8fc01fe15bcbee6495f6152b7d910215a
Kaif Ansari <ansarikaif51692@gmail.com>
2026-09-01 17:37:35 +0530

    Add Abstraction

---
+++ 7)Abstraction/intro.py
@@ -0,0 +1,15 @@
+# Abstraction (chupa hua): Hiding the implementation details of a class and only showing the essential features to the user.(unnecessary cheezein chupana)
+
+class Car:
+    def __init__(self):
+        self.acc=False
+        self.brk=False
+        self.clutch=False 
+
+    def start(self):
+        self.clutch=True
+        self.acc=True
+        print("Car started..")
+
+car1=Car()
+car1.start()
